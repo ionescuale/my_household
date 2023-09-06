e84a86a583b4d20204bcbf22c105c46b42665e79
Alexandra Ionescu <ionescu.ale@gmail.com>
2023-09-06 23:43:40 +0300

    first commit

---
+++ household/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+from django.template import loader
+
+
+# Create your views here.
+def index(request):
+    context = {}
+    html_template = loader.get_template("home/index.html")
+    return HttpResponse(html_template.render(context, request))
