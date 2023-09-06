ab04e90ba47ce3e25f07809d272c971ace76d946
Alexandra Ionescu <ionescu.ale@gmail.com>
2023-09-07 00:55:55 +0300

    first commit

--- household/models.py
+++ household/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Household(models.Model):
+
+    name = models.CharField(max_length=50)
+    description = models.TextField(max_length=500)
+    # Should I add here the user_id as foreign key? After creating teh userextend app
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
